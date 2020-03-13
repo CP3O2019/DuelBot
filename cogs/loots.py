50b7bf6dc80f0239635a3458977c4e9c305cb52d
Chris Porter <chris.is.a.porter@gmail.com>
2020-03-13 13:34:49 -0700

    added emojis

--- cogs/loots.py
+++ cogs/loots.py
@@ -25,6 +25,7 @@ class PotentialItems(commands.Cog):
         self.lootArray = {995: [0, ItemEmojis.Coins.coins]}
         self.totalPrice = 0
 
+    #Returns dict of loot
     async def rollLoot(self):
 
         lootDict = self.rollForLoot()
@@ -80,7 +81,7 @@ class PotentialItems(commands.Cog):
 
             print('LOOT VALUES:', lootDict[itemKey])
             # 0 is the name of the item, 1 is the integer value of the item, 2 is the item price shortened, 3 is the number of the item, 4 is the emoji
-            # self.lootArray[itemKey] = [jsonResponse['item']['name'], value, itemPrice, lootDict[itemKey][0], lootDict[itemKey][1]]
+            self.lootArray[itemKey] = [jsonResponse['item']['name'], value, itemPrice, lootDict[itemKey][0], lootDict[itemKey][1]]
         
         # Add the coin value of each item to the the total coins in the drop
         for item in self.lootArray.values():
