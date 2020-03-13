5e6673f692aa55b26a8095d88f6d17300acf6d8e
Chris Porter <chris.is.a.porter@gmail.com>
2020-03-13 13:47:03 -0700

    fixed dchain emoji

--- cogs/attackcommands.py
+++ cogs/attackcommands.py
@@ -250,7 +250,6 @@ class AttackCommands(commands.Cog):
                 commaMoney = "{:,d}".format(loot[995][1])
                 lootMessage += f"Total loot value: **{commaMoney} GP** {ItemEmojis.Coins.Coins}"
                 await lastmsg.edit(content=lootMessage)
-                await message.send(lootMessage)
 
     # Checking to see if the player who's turn it is has taken their turn
     # Takes in a message from the previous turn
