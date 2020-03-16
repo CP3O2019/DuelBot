81c1e46a83eca21e835104f54deae0cad43d76e2
Chris Porter <chris.is.a.porter@gmail.com>
2020-03-16 04:34:25 -0700

    fixed a typo

--- cogs/loots.py
+++ cogs/loots.py
@@ -121,12 +121,12 @@ class PotentialItems(commands.Cog):
                 value = int(itemPrice)
 
             print('LOOT VALUES:', lootDict[itemKey])
-            # 0 is the name of the item, 1 is the integer value of the item, 2 is the item price shortened, 3 is the number of the item, 4 is the emoji
+            # 0 is the name of the item, 1 is the GP (int) value of the item, 2 is the item price shortened, 3 is the number of the item, 4 is the emoji
             self.lootArray[itemKey] = [jsonResponse['item']['name'], value, itemPrice, lootDict[itemKey][0], lootDict[itemKey][1]]
         
         # Add the coin value of each item to the the total coins in the drop
         for item in self.lootArray.values():
-            self.lootArray[995][1] = self.lootArray[995][1] + item[1]
+            self.lootArray[995][1] = self.lootArray[995][1] + (item[1] * item[3])
 
         return self.lootArray
 
