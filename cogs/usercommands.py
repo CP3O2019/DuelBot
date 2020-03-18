9f386f12879e538c6e88cc82f78952350b055b87
Chris Porter <chris.is.a.porter@gmail.com>
2020-03-18 05:32:45 -0700

    added large dicing notifications

--- cogs/usercommands.py
+++ cogs/usercommands.py
@@ -269,8 +269,7 @@ class UserCommands(commands.Cog):
                 await message.send("You can't dice less than 1 GP.")
                 return
 
-            diceAmountString_lost = helper.shortNumify(diceAmount, 1)
-            diceAmountString_winnings = helper.shortNumify(diceAmount, 1)
+            diceAmountStrings = helper.shortNumify(diceAmount, 1)
 
             hasMoney = await helper.removeGPFromUser(message, message.author.id, diceAmount)
 
@@ -279,11 +278,19 @@ class UserCommands(commands.Cog):
 
             rand = randint(1, 100)
             diceDescription = ''
+            winStatus = ""
             if rand >= 55:
                 await helper.giveGPToUser(message, message.author.id, diceAmount * 2)
-                diceDescription = f'You rolled a **{rand}** and won {diceAmountString_winnings} GP'
+                diceDescription = f'You rolled a **{rand}** and won {diceAmountString} GP'
+                winStatus = "won"
             else:
-                diceDescription = f'You rolled a **{rand}** and lost {diceAmountString_lost} GP.'
+                diceDescription = f'You rolled a **{rand}** and lost {diceAmountString} GP.'
+                winStatus = "lost"
+
+            # If over 100M is diced, send it to the global notifications channel
+            if diceAmount >= 100000000:
+                notifChannel = await discord.get_channel(689313376286802026)
+                await notifChannel.send(f"{message.author.nick} has just diced **{diceAmountString_winnings}** and {winStatus}.")
 
             embed = discord.Embed(title='**Dice Roll**', description=diceDescription, color = discord.Color.orange())
             embed.set_thumbnail(url='https://vignette.wikia.nocookie.net/runescape2/images/f/f2/Dice_bag_detail.png/revision/latest?cb=20111120013858')
