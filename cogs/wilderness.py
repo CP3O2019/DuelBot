74d6573641ee40b48b4129c8759d446499a53a9e
Chris Porter <chris.is.a.porter@gmail.com>
2020-03-26 00:19:41 -0700

    .

--- cogs/wilderness.py
+++ cogs/wilderness.py
@@ -532,8 +532,11 @@ class Wilderness(commands.Cog):
             return
 
         timeStatus = await self.checkTripTime(ctx.author.id)
-        # print("Difference", math.floor(time.time()) - timeStatus[1])
+        
+        if timeStatus[0] == 'pking' and (math.floor(time.time()) - timeStatus[1]) > 1200:
+            await self.endTripSQL(ctx.author.id)
 
+        # Catch a situation where an user has never performed an activity
         if timeStatus[0] == 'idle' or timeStatus[0] == None: 
             await self.startTripSQL(ctx.author.id)
 
@@ -564,21 +567,14 @@ class Wilderness(commands.Cog):
 
         await ctx.send(f'You head out on a PK trip to {convertToLongName(args[0])}. You should return in 20 minutes or less.')
 
-        onCurrentTrip = True
         # Every 4 min, 59 secs attempt to pk another player
         for n in range(1, 4):
-            while onCurrentTrip == True:
-                savedTime = await self.checkTripTime(ctx.author.id)
 
                 # Wait 4 minutes, 55 seconds
                 await asyncio.sleep(4 * 59)
 
                 # Grab the user's status (were they pked?)
                 activityInfo = await self.checkTripTime(ctx.author.id)
-
-                if savedTime != activityInfo:
-                    onCurrentTrip = False
-                    return
 
                 # If they're still pking
                 if activityInfo[0] == 'pking':
