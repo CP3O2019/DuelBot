e3eb9cb2dea37d2fbe38b2ad9fc0ba895e248e76
Chris Porter <chris.is.a.porter@gmail.com>
2020-03-27 10:22:09 -0700

    Added highscores

--- cogs/highscores.py
+++ cogs/highscores.py
@@ -423,7 +423,7 @@ class Highscores(commands.Cog):
 
         await waitForReaction()
 
-    @stats.error
+    @search.error
     async def stats_error(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.send('To look up a player, type *.search [playerName]*')
