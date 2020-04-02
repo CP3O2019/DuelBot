6c9328069db1cce58a4b3637b043639b5fa989e4
Chris Porter <chris.is.a.porter@gmail.com>
2020-04-01 23:21:37 -0700

    .elder added, ranged added, new commands added

--- cogs/slayer.py
+++ cogs/slayer.py
@@ -947,7 +947,7 @@ class Slayer(commands.Cog):
 
         await ctx.send(f"You begin slaying {taskMonster.name}s. You should return in about {math.ceil(30 * (numberKilled/maxKills))} minutes.")
 
-        await asyncio.sleep(math.floor(10 * (numberKilled/maxKills)))
+        await asyncio.sleep(math.floor(1800 * (numberKilled/maxKills)))
 
         # Remove the monsters from the task monster count
         await removeFromTask(ctx.author.id, numberKilled)
