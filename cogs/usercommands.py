0f75e76663ae02a1bc02d0ae06a71e4758cca4f2
Chris Porter <chris.is.a.porter@gmail.com>
2020-04-01 23:45:09 -0700

    .elder added, ranged added, new commands added

--- cogs/usercommands.py
+++ cogs/usercommands.py
@@ -37,7 +37,7 @@ class UserCommands(commands.Cog):
     async def server(self, ctx):
         await ctx.send("""
         **Click the link below to join our server and get an extra loot roll every kill!**
-        https://discord.gg/cA7Cejz
+        https://discord.gg/TNaj6CG
         """)
 
     async def createTablesForUser(self, user):
